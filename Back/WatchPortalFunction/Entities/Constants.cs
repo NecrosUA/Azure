18d54db065dffa195bc840dc41b5f6279abcb0e5
NecrosUA <rostislav.email@gmail.com>
2022-06-08 09:44:59 +0200

    commit b2c-javascript-test

---
+++ Back/WatchPortalFunction/Entities/Constants.cs
@@ -0,0 +1,8 @@
+﻿
+namespace WatchPortalFunction.Entities
+{
+    public class Constants
+    {
+        public static readonly string SECRET_KEY = "ABRACADABRA_MAGIC_SECRET_KEY_JUST_A_LONG_STRING";
+    }
+}
---
+++ Back/WatchPortalFunction/Entities/WatchInfoEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WatchPortalFunction.Entities
+{
+    public class WatchInfoEntity
+    {
+        public string Manufacturer { get; set; }
+        public string CaseType { get; set; }
+        public string Bezel { get; set; }
+        public string Dial { get; set; }
+        public string CaseFinish { get; set; }
+        public int? Jewels { get; set; }
+    }
+}
